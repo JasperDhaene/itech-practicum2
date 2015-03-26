f1be8b65969e1df70defa84ee9e49050fc43203c
jaudhaen <jasper.dhaene@ugent.be>
2015-03-26 14:14:15 +0100

    added event edit/delete menu button

--- app/src/main/java/be/thalarion/eventman/ShowEventActivity.java
+++ app/src/main/java/be/thalarion/eventman/ShowEventActivity.java
@@ -1,5 +1,6 @@
 package be.thalarion.eventman;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,7 +8,11 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+
+import be.thalarion.eventman.api.APIException;
 import be.thalarion.eventman.models.Event;
+import be.thalarion.eventman.models.Model;
 
 
 public class ShowEventActivity extends ActionBarActivity {
@@ -43,22 +48,44 @@ public class ShowEventActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_show_event, menu);
+        getMenuInflater().inflate(R.menu.event, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.action_edit_event:
+                //TODO:change this so that it goes to 'edit' instead of 'new' person
+                intent = new Intent(this, EditEventActivity.class);
+                intent.putExtra("event", this.event);
+                intent.putExtra("action", "edit");
+                startActivity(intent);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                break;
+            case R.id.action_discard_event:
+
+                new Thread(new Runnable() {
+                    public void run() {
+                        try {
+                            ((Model) event).destroy();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (APIException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+
+                //TODO: load the eventFragment here
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+
+                break;
+            default:
+                return false;
         }
-
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 }
