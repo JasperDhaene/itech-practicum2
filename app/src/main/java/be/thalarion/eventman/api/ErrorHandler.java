b5f8454adde8cb5208db4186721f1d79c41e5765
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-03-29 11:11:22 +0200

    Added debug and release error handling

--- app/src/main/java/be/thalarion/eventman/api/ErrorHandler.java
+++ app/src/main/java/be/thalarion/eventman/api/ErrorHandler.java
@@ -3,18 +3,32 @@ package be.thalarion.eventman.api;
 import android.content.Context;
 import android.widget.Toast;
 
+import java.io.IOException;
+
+import be.thalarion.eventman.BuildConfig;
+import be.thalarion.eventman.R;
+
 public class ErrorHandler {
 
     /**
      * announce - Handle exceptions gracefully
      * @param context
      * @param e The exception to handle
-     * @return boolean Whether or not the exception is fatal (operation-local)
      */
-    public static boolean announce(Context context, Exception e) {
-        Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG);
-        e.printStackTrace();
-        return true;
+    public static void announce(Context context, Exception e) {
+        if(BuildConfig.DEBUG) {
+            Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG);
+            e.printStackTrace();
+        } else {
+            try {
+                throw e;
+            } catch (APIException err) {
+                Toast.makeText(context, context.getResources().getString(R.string.error_api), Toast.LENGTH_LONG);
+            } catch (IOException err) {
+                Toast.makeText(context, context.getResources().getString(R.string.error_io), Toast.LENGTH_LONG);
+            } catch (Exception err) {
+                Toast.makeText(context, context.getResources().getString(R.string.error_unknown), Toast.LENGTH_LONG);
+            }
+        }
     }
-
 }
