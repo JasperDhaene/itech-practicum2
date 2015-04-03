21b61d87503b2488b67ebeeff702308195dc7e07
jaudhaen <jasper.dhaene@ugent.be>
2015-04-03 10:21:48 +0200

    styled list event

--- app/src/main/java/be/thalarion/eventman/adapters/EventsAdapter.java
+++ app/src/main/java/be/thalarion/eventman/adapters/EventsAdapter.java
@@ -1,28 +1,23 @@
 package be.thalarion.eventman.adapters;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.amulyakhare.textdrawable.TextDrawable;
 
-import org.parceler.Parcels;
-
 import java.util.ArrayList;
 import java.util.List;
 
-import be.thalarion.eventman.EditEventDialogFragment;
 import be.thalarion.eventman.R;
 import be.thalarion.eventman.ShowEventFragment;
-import be.thalarion.eventman.ShowPersonFragment;
 import be.thalarion.eventman.models.Event;
-import be.thalarion.eventman.models.Model;
 import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
 
 public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder> {
@@ -53,15 +48,11 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder
             this.description = ((TextView) itemView.findViewById(R.id.event_list_view_description));
             this.avatar = ((ImageView) itemView.findViewById(R.id.event_list_view_avatar));
 
-            ((CardView) itemView.findViewById(R.id.card)).setOnClickListener(this);
+            ((LinearLayout) itemView.findViewById(R.id.list_item_container)).setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
-            /*Intent intent = new Intent(v.getContext(), ShowEventActivity.class);
-            intent.putExtra("event", Parcels.wrap(this.event));
-            v.getContext().startActivity(intent);*/
-
 
             ((MaterialNavigationDrawer) v.getContext()).setFragmentChild(
                     ShowEventFragment.newInstance(this.event),
@@ -75,7 +66,7 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder
     public EventsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
         View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.card_event, parent, false);
+                .inflate(R.layout.list_item_event, parent, false);
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
@@ -85,21 +76,21 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder
     public void onBindViewHolder(ViewHolder holder, int position) {
         String color;
         if(dataSet.get(position).getTitle() != null) {
-            holder.title.setTextAppearance(context, R.style.Title);
+            //holder.title.setTextAppearance(context, R.style.Title);
             holder.title.setText(dataSet.get(position).getTitle());
 
             color = Event.hash(dataSet.get(position).getTitle());
         } else {
-            holder.title.setTextAppearance(context, R.style.TitleMissing);
+            //holder.title.setTextAppearance(context, R.style.TitleMissing);
             holder.title.setText(R.string.error_text_notitle);
 
             color = Event.hash("Ev");
         }
         if(dataSet.get(position).getDescription() != null) {
-            holder.description.setTextAppearance(context, R.style.SubTitle);
+            //holder.description.setTextAppearance(context, R.style.SubTitle);
             holder.description.setText(dataSet.get(position).getDescription());
         } else {
-            holder.description.setTextAppearance(context, R.style.SubTitleMissing);
+           // holder.description.setTextAppearance(context, R.style.SubTitleMissing);
             holder.description.setText(R.string.error_text_nodescription);
         }
 
