6af854d3d1cb2344db17d6a891f88ee42782bb31
jaudhaen <jasper.dhaene@hotmail.com>
2015-04-08 18:59:24 +0200

    added styling to messages

--- app/src/main/java/be/thalarion/eventman/adapters/MessagesAdapter.java
+++ app/src/main/java/be/thalarion/eventman/adapters/MessagesAdapter.java
@@ -20,6 +20,7 @@ import java.util.List;
 import be.thalarion.eventman.MainActivity;
 import be.thalarion.eventman.R;
 import be.thalarion.eventman.ShowPersonFragment;
+import be.thalarion.eventman.models.Event;
 import be.thalarion.eventman.models.Message;
 import be.thalarion.eventman.models.Person;
 import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
@@ -45,21 +46,18 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHo
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView text;
+        public TextView text,date;
+
         public ImageView avatar;
         public LinearLayout container;
         public Message message;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            this.text = ((TextView) itemView.findViewById(R.id.message_text));
-            /*
-            this.avatar = ((ImageView) itemView.findViewById(R.id.person_list_view_avatar));
-            this.container = ((LinearLayout) itemView.findViewById(R.id.list_item_container));
+            this.text = ((TextView) itemView.findViewById(R.id.message_list_view_text));
+            this.avatar = ((ImageView) itemView.findViewById(R.id.message_list_view_avatar));
+            this.date = ((TextView) itemView.findViewById(R.id.message_list_view_date));
 
-
-
-            this.container.setOnClickListener(this);*/
         }
 
 
@@ -86,6 +84,20 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHo
             holder.text.setText(R.string.error_text_noname);
         }
 
+        if(dataSet.get(position).getPerson() != null) {
+            ImageLoader.getInstance().loadImage(dataSet.get(position).getPerson().getAvatar(Person.AVATAR.THUMB), new SimpleImageLoadingListener() {
+                @Override
+                public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                    holder.avatar.setImageBitmap(loadedImage);
+                }
+            });
+            //TODO:what if person is null?
+        }
+
+        if(dataSet.get(position).getDate() != null) {
+
+            holder.date.setText(Event.format.format(dataSet.get(position).getDate()).toString());
+        }
 
         holder.message = dataSet.get(position);
     }
